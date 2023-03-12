895df64784e5a0a01996e936e60d3041ef1f99ba
Jorge Luis Serna Velasquez <jorgelserve@icloud.com>
2023-03-12 19:19:13 +0000

    Interrupt rtc

--- src/test.cpp
+++ src/test.cpp
@@ -1,8 +1,15 @@
 #include <Arduino.h>
-#include <TimerOne.h>
 #include <Wire.h>                       // For some strange reasons, Wire.h must be included here
 #include <DS1307new.h>
 
+
+// *********************************************
+// DEFINE
+// *********************************************
+
+// *********************************************
+// VARIABLES
+// *********************************************
 uint16_t startAddr = 0x0000;            // Start address to store in the NV-RAM
 uint16_t lastAddr;                      // new address for storing in NV-RAM
 uint16_t TimeIsSet = 0xaa55;            // Helper that time must not set again
@@ -14,20 +21,57 @@ int hour, minute, second;
 int year, day;
 char month[3];
 
-void timerIsr(void);
+struct Horario {
+  byte hora;
+  byte minuto;
+  byte segundos;
+};
+
+Horario horarios[] = {
+  {8, 0},
+  {12, 30},
+  {17, 45}
+};
+
+
+
 int getMonthFromAbbreviation(char*);
 
+// *********************************************
+// SETUP
+// *********************************************
 void setup() {
-  Serial.begin(115200); // Inicializar el puerto serie a 9600 baudios
+  pinMode(2, INPUT);                    // Test of the SQW pin, D2 = INPUT
+  digitalWrite(2, HIGH);                // Test of the SQW pin, D2 = Pullup on
 
-  RTC.setRAM(0, (uint8_t *)&startAddr, sizeof(uint16_t));
+  Serial.begin(9600);
+
+/*
+   PLEASE NOTICE: WE HAVE MADE AN ADDRESS SHIFT FOR THE NV-RAM!!!
+                  NV-RAM ADDRESS 0x08 HAS TO ADDRESSED WITH ADDRESS 0x00=0
+                  TO AVOID OVERWRITING THE CLOCK REGISTERS IN CASE OF
+                  ERRORS IN YOUR CODE. SO THE LAST ADDRESS IS 0x38=56!
+*/
+  RTC.setRAM(0, (uint8_t *)&startAddr, sizeof(uint16_t));// Store startAddr in NV-RAM address 0x08
+
+/*
+   Uncomment the next 2 lines if you want to SET the clock
+   Comment them out if the clock is set.
+   DON'T ASK ME WHY: YOU MUST UPLOAD THE CODE TWICE TO LET HIM WORK
+   AFTER SETTING THE CLOCK ONCE.
+*/
   TimeIsSet = 0xffff;
   RTC.setRAM(54, (uint8_t *)&TimeIsSet, sizeof(uint16_t));
 
-
+/*
+  Control the clock.
+  Clock will only be set if NV-RAM Address does not contain 0xaa.
+  DS1307 should have a battery backup.
+*/
   RTC.getRAM(54, (uint8_t *)&TimeIsSet, sizeof(uint16_t));
   Serial.println(compileTime);
-  if (TimeIsSet != 0xaa55) {
+  if (TimeIsSet != 0xaa55)
+  {
     RTC.stopClock();
 
     // month = getMonthFromAbbreviation(compileDate);
@@ -44,7 +88,9 @@ void setup() {
     TimeIsSet = 0xaa55;
     RTC.setRAM(54, (uint8_t *)&TimeIsSet, sizeof(uint16_t));
     RTC.startClock();
-  } else {
+  }
+  else
+  {
     RTC.getTime();
   }
 
@@ -54,28 +100,30 @@ void setup() {
   RTC.ctrl = 0x00;                      // 0x00=disable SQW pin, 0x10=1Hz,
                                         // 0x11=4096Hz, 0x12=8192Hz, 0x13=32768Hz
   RTC.setCTRL();
-
-
-  Timer1.initialize(1000000); // Configurar el temporizador para generar una interrupción cada segundo (1000000 microsegundos = 1 segundo)
-  Timer1.attachInterrupt(timerIsr); // Asignar la rutina de interrupción al temporizador
 }
 
-void loop() {
-}
+// *********************************************
+// MAIN (LOOP)
+// *********************************************
+void loop()
+{
 
-int getMonthFromAbbreviation(char monthAbbreviation[3]) {
-  // Función auxiliar para convertir la abreviatura del mes en el número del mes
-  const char *monthNames[] = {"", "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
-  for (int i = 1; i <= 12; i++) {
-    if (strcmp(monthAbbreviation, monthNames[i]) == 0) {
-      return i;
+
+
+
+
+byte hora_actual = RTC.hour;
+  byte minuto_actual = RTC.minute;
+  
+  for (unsigned int i = 0; i < sizeof(horarios)/sizeof(horarios[0]); i++) {
+    if (horarios[i].hora == hora_actual && horarios[i].minuto == minuto_actual) {
+      digitalWrite(LED_BUILTIN, HIGH); // encender LED
+      break;
     }
   }
-  return 0;  // Si no se encuentra el mes, se devuelve 0
-}
 
-void timerIsr() {
-  Serial.println("Hola, mundo!"); // Mostrar un saludo por el puerto serie
+
+
 
   RTC.getTime();
   if (RTC.hour < 10)                    // correct hour if necessary
@@ -154,4 +202,26 @@ void timerIsr() {
       Serial.print("SUN");
       break;
   }
+
+
+  // if (TimeIsSet == 0xaa55)              // check if the clock was set or not
+  // {
+  //   Serial.println(" - Clock was set!");
+  // }
+  // else
+  // {
+  //   Serial.println(" - Clock was NOT set!");
+  // }
+  delay(1000);                          // wait a second
+}
+
+int getMonthFromAbbreviation(char monthAbbreviation[3]) {
+  // Función auxiliar para convertir la abreviatura del mes en el número del mes
+  const char *monthNames[] = {"", "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
+  for (int i = 1; i <= 12; i++) {
+    if (strcmp(monthAbbreviation, monthNames[i]) == 0) {
+      return i;
+    }
+  }
+  return 0;  // Si no se encuentra el mes, se devuelve 0
 }
